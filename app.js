0454453df4309565e8082f660fb6a6e72df5fd71
su-de-sh <sudesh7443@gmail.com>
2022-08-03 16:09:32 +0545

    refactor: move model in models/blog.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const cors = require("cors");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// });
 
-const Blog = mongoose.model("Blog", blogSchema);
+// const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
