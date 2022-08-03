1847ef7e680b2fd86be5ab381a31d8d6f0c6e35a
su-de-sh <sudesh7443@gmail.com>
2022-08-03 17:23:42 +0545

    fix: remove extra comments

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ const config = require("./utils/config");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogrouter");
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
-
-// const Blog = mongoose.model("Blog", blogSchema);
-
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
