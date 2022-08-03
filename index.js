c030d000e8f1864c1c96ceb8d73c4f61e3278a59
su-de-sh <sudesh7443@gmail.com>
2022-08-03 15:12:31 +0545

    refactor: refactor codes to different modules

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require("http");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const app = require("./app");
+const config = require("./utils/config");
 
-const Blog = mongoose.model('Blog', blogSchema)
+const server = http.createServer(app);
 
-const mongoUrl = 'mongodb+srv://sudesh:12313mangodb@cluster0.ej4lg.mongodb.net/Blog?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+server.listen(config.PORT, () => {
+  console.log(`Server running at ${config.PORT}`);
+});
