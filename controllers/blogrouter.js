31434dffadba48341f724b16ed213ec2da9592ae
su-de-sh <sudesh7443@gmail.com>
2022-08-06 22:29:37 +0545

    fix: add try catch to catch probable errors

--- controllers/blogrouter.js
+++ controllers/blogrouter.js
@@ -1,43 +1,59 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", async (req, res) => {
-  const data = await Blog.find();
+blogRouter.get("/", async (req, res, next) => {
+  try {
+    const data = await Blog.find();
 
-  res.status(200).json(data);
-});
-
-blogRouter.post("/", async (req, res) => {
-  if (req.body.likes === undefined) {
-    req.body.likes = 0;
-  }
-  if (!(req.body.title && req.body.url)) {
-    res.status(400).end();
-  } else {
-    const blog = new Blog(req.body);
-    const data = await blog.save();
-    res.status(201).json(data);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
   }
 });
 
-blogRouter.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  await Blog.findByIdAndRemove(id);
-  res.status(204).end();
+blogRouter.post("/", async (req, res, next) => {
+  try {
+    if (req.body.likes === undefined) {
+      req.body.likes = 0;
+    }
+    if (!(req.body.title && req.body.url)) {
+      res.status(400).end();
+    } else {
+      const blog = new Blog(req.body);
+      const data = await blog.save();
+      res.status(201).json(data);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogRouter.put("/:id", async (req, res) => {
-  const newBlog = {
-    title: req.body.title,
-    author: req.body.author,
-    url: req.body.url,
-    likes: req.body.likes,
-  };
+blogRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    await Blog.findByIdAndRemove(id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
-  const data = await Blog.findByIdAndUpdate(req.params.id, newBlog, {
-    new: true,
-  });
-  res.status(200).json(data);
+blogRouter.put("/:id", async (req, res, next) => {
+  try {
+    const newBlog = {
+      title: req.body.title,
+      author: req.body.author,
+      url: req.body.url,
+      likes: req.body.likes,
+    };
+
+    const data = await Blog.findByIdAndUpdate(req.params.id, newBlog, {
+      new: true,
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = blogRouter;
