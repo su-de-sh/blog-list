365cb007b9094a5d1ad55f26700ff05817269614
su-de-sh <sudesh7443@gmail.com>
2022-08-06 16:33:40 +0545

    fix: change promise to async and await

--- controllers/blogrouter.js
+++ controllers/blogrouter.js
@@ -1,16 +1,22 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (req, res) => {
-  Blog.find().then((data) => res.status(200).json(data));
+blogRouter.get("/", async (req, res) => {
+  const data = await Blog.find();
+
+  res.status(200).json(data);
 });
 
-blogRouter.post("/", (req, res) => {
+blogRouter.post("/", async (req, res) => {
   if (req.body.likes === undefined) {
     req.body.likes = 0;
   }
+  if (!(req.body.title && req.body.url)) {
+    res.status(400).end();
+  }
   const blog = new Blog(req.body);
-  blog.save().then((data) => res.status(201).json(data));
+  const data = await blog.save();
+  res.status(201).json(data);
 });
 
 module.exports = blogRouter;
